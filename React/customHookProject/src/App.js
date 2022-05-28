d005fbe9bf2f7150cbb6357a93c6a39c829739c3
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-05-28 22:54:46 +0530

    first commit

---
+++ React/customHookProject/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState} from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+ 
+
+  const httpDAta = useHttp();
+  const { isLoading, error, sendRequest: fetchTasks } = httpDAta;
+
+  useEffect(() => {
+    const transformTask = ((taskObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    });
+
+    fetchTasks({
+      url: "https://customhook-feefb-default-rtdb.firebaseio.com/tasks.json",
+    }, transformTask);
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ React/customHookProject/src/hooks/use-http.js
@@ -0,0 +1,53 @@
+import { useCallback, useState } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback (async (requestConfig,applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+            requestConfig.url, {
+                method:requestConfig.method ? requestConfig.method : 'GET',
+                headers:requestConfig.headers ? requestConfig.headers : {},
+                body:requestConfig.body ?  JSON.stringify(requestConfig.body) : null,
+            }
+        );
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+        applyData(data);
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+    //   setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  },[]);
+
+  return {
+      isLoading,
+      error,
+      sendRequest,
+  }
+
+//   useEffect(() => {
+//     fetchTasks();
+//   }, []);
+
+//   const taskAddHandler = (task) => {
+//     setTasks((prevTasks) => prevTasks.concat(task));
+//   };
+};
+
+export default useHttp;
