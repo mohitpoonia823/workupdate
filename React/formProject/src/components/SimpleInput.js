d76aa0095d6cfb341471b707da071d38fcfb29e4
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-05-31 22:48:14 +0530

    first commit

---
+++ React/formProject/src/components/SimpleInput.js
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsvalid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsvalid(false);
+      return;
+    }
+    setEnteredNameIsvalid(true);
+    console.log(enteredName);
+    const enteredNameValue = nameInputRef.current.value;
+
+    console.log(enteredNameValue);
+    setEnteredName("");
+  };
+
+  const nameInpoutIsValid = !enteredNameIsValid && enteredNameTouched
+const nameInputClass = nameInpoutIsValid ? 'form-control invalid' : 'form-control '
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClass}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInpoutIsValid && (
+          <p className="error-text">Please enter a valid inpute</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
