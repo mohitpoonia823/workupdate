fc9f66e5f7c5f73d492c33a4c5a1495c60287256
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-06-03 21:49:47 +0530

    first commit

---
+++ React/reduxFirstProject/src/App.js
@@ -0,0 +1,10 @@
+import Counter from './components/Counter';
+import React from 'react';
+
+function App() {
+  return (
+    <Counter />
+  );
+}
+
+export default App;
---
+++ React/reduxFirstProject/src/store/index.js
@@ -0,0 +1,42 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0, showCounter: true };
+ 
+const createReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter
+
+    };
+  }
+
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter
+
+    };
+  }
+
+  if(action.type === 'toogle'){
+      return{
+          showCounter:!state.showCounter,
+          counter: state.counter
+      }
+  }
+
+  return state;
+};
+
+const store = createStore(createReducer);
+
+export default store;
