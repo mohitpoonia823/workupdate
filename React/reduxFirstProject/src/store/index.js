0ee3a91ab8021372705193c60ad196881c4c4448
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-06-06 23:04:51 +0530

    first commit

--- React/reduxFirstProject/src/store/index.js
+++ React/reduxFirstProject/src/store/index.js
@@ -1,42 +1,30 @@
-import { createStore } from "redux";
+import {createSlice, configureStore} from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name:'counter',
+  initialState:initialState,
+  reducers:{
+    increment(state){
+      state.counter++;
+    },
+    decrement(state){
+      state.counter--;
+    },
+    increase(state, action){
+      state.counter = state.counter + action.amount;
+    },
+    toogleCounter(state){
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
  
-const createReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
-  }
-
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter
-
-    };
-  }
 
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-
-    };
-  }
-
-  if(action.type === 'toogle'){
-      return{
-          showCounter:!state.showCounter,
-          counter: state.counter
-      }
-  }
-
-  return state;
-};
-
-const store = createStore(createReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
 export default store;
