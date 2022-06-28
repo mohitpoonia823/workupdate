a9f21010d9d5d1811eeec70f77ffa19ae0a48c1a
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-06-28 06:53:29 +0530

    first commit

--- React/replaceRedux/src/index.js
+++ React/replaceRedux/src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+import configureProductsStore from './hooks-store/products-store';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+configureProductsStore();
 
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
