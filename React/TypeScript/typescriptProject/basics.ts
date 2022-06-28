1c154f412c6758a305fa995220748c22ad2b70d0
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-06-28 18:11:49 +0530

    first commit

---
+++ React/TypeScript/typescriptProject/basics.ts
@@ -0,0 +1,38 @@
+
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Max',
+  age: 32
+};
+
+// person = {
+//   isEmployee: true
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
