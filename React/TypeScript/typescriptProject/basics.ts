b6ca6eef7b344d3da11d1643e4df4c1e67566d2a
mohitpoonia823 <mohitpoonia823@gmail.com>
2022-06-29 22:16:49 +0530

    first commit

--- React/TypeScript/typescriptProject/basics.ts
+++ React/TypeScript/typescriptProject/basics.ts
@@ -1,10 +1,8 @@
-
-
 let age: number;
 
 age = 12;
 
-let userName: string;
+let userName: string | string[];
 
 userName = 'Max';
 
@@ -18,10 +16,12 @@ let hobbies: string[];
 
 hobbies = ['Sports', 'Cooking'];
 
-let person: {
+type Person = {
   name: string;
   age: number;
 };
+
+let person: Person;
 
 person = {
   name: 'Max',
@@ -32,7 +32,34 @@ person = {
 //   isEmployee: true
 // };
 
-let people: {
-  name: string;
-  age: number;
-}[];
+let people: Person[];
+
+// Type inference
+
+let course: string | number = 'React - The Complete Guide';
+
+course = 12341;
+
+// Functions & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
+
+// updatedArray[0].split('');
