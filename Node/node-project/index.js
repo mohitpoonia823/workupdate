fd665fe9c05d9fd5bc3c46864704db2700b41526
Mohit Poonia <mohit.poonia@meetribbon.com>
2022-11-30 09:56:58 +0530

    Node course

--- Node/node-project/index.js
+++ Node/node-project/index.js
@@ -16,7 +16,7 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 // Non-blocking, asynchronous way
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   if (err) return console.log('ERROR! 💥');
+//   if (err) return console.log('ERROR! ');
 
 //   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //     console.log(data2);
@@ -24,7 +24,7 @@ const replaceTemplate = require('./modules/replaceTemplate');
 //       console.log(data3);
 
 //       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//         console.log('Your file has been written 😁');
+//         console.log('Your file has been written ');
 //       })
 //     });
 //   });
